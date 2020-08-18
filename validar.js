8d6f387b78dc8cf162d84df53a5dfd9a315b9df7
yta <ytasegundo@gmail.com>
2020-08-18 17:55:42 -0400

    primer commit validando campos y saludo

---
+++ validar.js
@@ -0,0 +1,75 @@
+function validar(){
+    var nombre;
+    nombre = document.getElementById("nombre").value;
+
+    if (nombre === ""){
+        alert("Todos los campos son obligatorios");
+        return false;
+    }
+    else if (nombre.length>30){
+        alert("El nombre es muy largo");
+        return false;
+    }
+
+    
+
+    var apellidos;
+    apellidos= document.getElementById("apellidos").value;
+
+    if (apellidos === ""){
+        alert("todos los campos son obligatorios");
+        return false;
+    }
+    else if (nombre.lenght>80){
+        alert("tus apellidos son muy largos sólo 30 caracteres");
+        return false;
+    }
+    
+
+    var correo;
+    correo= document.getElementById("correo").value;
+
+    expresion = /\w+@\w+\.+[a-z]/;
+    if(correo===""){
+        alert("campos obligatorios");
+        return false;
+    }
+    else if(!expresion.test(correo)){
+        alert("correo no válido");
+        return false;
+    }
+    else if (correo.lenght>100){
+        alert("correo muy largo");
+        return false;
+    }
+
+    var usuario;
+    usuario= document.getElementById("usuario").value;
+    if(usuario===""){
+        alert("campo requerido");
+        return false;
+    }
+    else if (usuario.lenght>20){
+        alert("usuario muy largo");
+        return false;
+    }
+    
+
+    var telefono;
+    telefono= document.getElementById("telefono").value;
+    if(telefono===""){
+        alert("campo requerido");
+        return false;
+    }
+    else if(telefono.lenght>15){
+        alert("máximo 15 carácteres");
+        return false;
+    }
+    else if(telefono=="string"){
+        alert("sólo carácteres numéricos");
+        return false;
+    }
+    var saludo="Hola"+" "+nombre+" "+ apellidos+" gracias por registrarte con nosotros!";
+
+    alert(saludo);
+}
